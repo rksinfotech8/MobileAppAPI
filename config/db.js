d0a602a6393351b028375fb9cc24f19a67786a5a
rksinfotech8 <rksinfotech8@gmail.com>
2025-02-12 11:25:40 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const sql = require("mssql");
+
+const config = {
+  user: process.env.DB_USER || "apsar11_",
+  password: process.env.DB_PASSWORD || "admin@123",
+  server: process.env.DB_SERVER || "sql.bsite.net\MSSQL2016", 
+  database: process.env.DB_NAME || "apsar11_",
+  options: {
+    encrypt: false, 
+    trustServerCertificate: true,
+  },
+};
+
+const connectDB = async () => {
+  try {
+    if (!sql.connected) {
+      await sql.connect(config);
+      console.log("✅ Database Connected");
+    }
+  } catch (err) {
+    console.error("❌ Database Connection Failed:", err.message);
+  }
+};
+
+module.exports = { connectDB, sql };
